70b2157c1eaeb13a5836c0f4a6518f70e60aee20
soldesk <soldesk@3-2>
2016-07-25 16:36:02 +0900

    resume_강부철 and personalmembermgr

---
+++ ITClan/src/main/java/spring/model/personalmember/PersonalMemberMgr.java
@@ -0,0 +1,20 @@
+package spring.model.personalmember;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import spring.model.resumeinfo.ResumeInfoMgr;
+
+public class PersonalMemberMgr {
+	
+	@Autowired
+	private PersonalMemberDAO dao;
+	@Autowired
+	private ResumeInfoMgr mgr;
+	
+    public void delete(String memberID , Object pk) throws Exception{
+    	 
+        mgr.delete(memberID , pk);
+        dao.delete(memberID);
+ }
+
+}
