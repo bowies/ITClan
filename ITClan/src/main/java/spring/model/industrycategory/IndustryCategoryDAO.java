ea049ee04d54c57d6206609f82a0cc398c551f18
soldesk <soldesk@3-8>
2016-07-25 16:22:59 +0900

    di! 개짓는소리좀 안나게 해라!

---
+++ ITClan/src/main/java/spring/model/industrycategory/IndustryCategoryDAO.java
@@ -0,0 +1,72 @@
+package spring.model.industrycategory;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import spring.model.itclan.DAOMyBatisInter;
+
+public  class IndustryCategoryDAO implements DAOMyBatisInter {
+
+	
+private SqlSessionTemplate mybatis;
+	
+	
+	public SqlSessionTemplate getMybatis() {
+		return mybatis;
+	}
+
+	public void setMybatis(SqlSessionTemplate mybatis) {
+		this.mybatis = mybatis;
+	}
+
+	
+	
+	@Override
+	public int create(Object dto) throws Exception {
+		return  mybatis.insert("industrycategory.create", dto);
+		
+	
+	
+	}
+
+	@Override
+	public List list(Map map) throws Exception {
+		// TODO Auto-generated method stub
+		return mybatis.selectList("industrycategory.list", map);
+	}
+
+	@Override
+	public Object read(Object pk) throws Exception {
+		// TODO Auto-generated method stub
+	    return mybatis.selectOne("industrycategory.read", pk);
+	}
+
+
+
+	@Override
+	public int delete(Object pk) throws Exception {
+		// TODO Auto-generated method stub
+		
+			return mybatis.delete("industrycategory.delete", pk);
+			
+		
+		  
+	}
+
+	@Override
+	public int update(Object dto) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int total(Map map) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+}
